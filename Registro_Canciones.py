b24176b7207771ffd1838defe84f6765dfb7bb79
Jose Enrique Flores Espinoza <72504407@continental.edu.pe>
2023-03-29 15:13:00 -0500

    creacion de menu de opciones

--- Registro_Canciones.py
+++ Registro_Canciones.py
@@ -5,4 +5,23 @@ canciones = {
     "Autor" : "Enrique Iglesias"
 }
 
-print(canciones)
+
+while True:
+    print("----Registro de canciones---\n")
+    print("1. Insetar cancion\n")
+    print("2. Modificar cancion\n")
+    print("3. Eliminar cancion\n")
+    print("4. Eliminar cancion\n")
+
+    opcion =str(input("Escoja una opcion:"))
+    if opcion == "1":
+        print("opcion 1")
+    elif opcion == "2":
+        print("opcion 2")
+    elif opcion == "2":
+        print("opcion 3")
+    elif opcion == "3":
+        print("opcion 3")
+    elif opcion == "4":
+        break
+
