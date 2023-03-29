771fe0e127d753f2e039758b43bcec55cc4ed317
Jose Enrique Flores Espinoza <72504407@continental.edu.pe>
2023-03-29 15:42:18 -0500

    creacion del modulo insertar

--- Registro_Canciones.py
+++ Registro_Canciones.py
@@ -1,10 +1,22 @@
 canciones = {
-    "cantante" : "Enrique Iglesias",
-    "Album" : "Los corazones",
-    "Año" : 2013,
-    "Autor" : "Enrique Iglesias"
+    "Cantante" : [],
+    "Album" : [],
+    "Año" : [],
+    "Autor" : []
 }
-
+def insertar():
+    tamaño = int(input("Cuantas canciones Ingresaras: "))
+    for i in range(tamaño):
+        cantate = input("Ingrese el Cantante {}: \t".format(i+1))
+        album = input("Ingrese el Album {}: \t".format(i+1))
+        año = input("Ingrese el Año {}: \t".format(i+1))
+        autor = input("Ingrese el Autor {}: \t".format(i+1))
+        canciones["Cantante"].append(cantate)
+        canciones["Album"].append(album)
+        canciones["Año"].append(año)
+        canciones["Autor"].append(autor)
+    print(canciones)
+    
 
 while True:
     print("----Registro de canciones---\n")
@@ -15,7 +27,7 @@ while True:
 
     opcion =str(input("Escoja una opcion:"))
     if opcion == "1":
-        print("opcion 1")
+        insertar()
     elif opcion == "2":
         print("opcion 2")
     elif opcion == "2":
